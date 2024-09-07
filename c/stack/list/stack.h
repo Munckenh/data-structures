1a3a38b1023bf9f8af136583a2a04ebbae57d576
Munckenh <76769232+Munckenh@users.noreply.github.com>
2024-09-07 18:39:54 +0800

    Add `stack.h`

---
+++ c/stack/list/stack.h
@@ -0,0 +1,21 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+struct stack {
+    struct node* top;
+};
+
+void initialize(struct stack*);
+bool is_empty(struct stack*);
+void push(struct stack*, int);
+int pop(struct stack*);
+int peek(struct stack*);
+
+#endif
