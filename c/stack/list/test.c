6af951b033816baa97db93b7db5308ec2616bc03
Munckenh <76769232+Munckenh@users.noreply.github.com>
2024-10-06 21:52:50 +0800

    Add test case for `sort()` function

--- c/stack/list/test.c
+++ c/stack/list/test.c
@@ -4,19 +4,30 @@
 #include "stack.h"
 
 void test() {
-    struct stack* stack;
-    initialize(stack);
-    assert(stack->top == NULL);
-    assert(is_empty(stack));
-    push(stack, 10);
-    assert(!is_empty(stack));
-    assert(peek(stack) == 10);
-    push(stack, 20);
-    assert(peek(stack) == 20);
-    assert(pop(stack) == 20);
-    assert(peek(stack) == 10);
-    assert(pop(stack) == 10);
-    assert(pop(stack) == INT_MAX);
+    struct stack stack;
+    initialize(&stack);
+    assert(stack.top == NULL);
+    assert(is_empty(&stack));
+    push(&stack, 10);
+    assert(!is_empty(&stack));
+    assert(peek(&stack) == 10);
+    push(&stack, 20);
+    assert(peek(&stack) == 20);
+    assert(pop(&stack) == 20);
+    assert(peek(&stack) == 10);
+    assert(pop(&stack) == 10);
+    assert(pop(&stack) == INT_MAX);
+    push(&stack, 50);
+    push(&stack, 20);
+    push(&stack, 40);
+    push(&stack, 10);
+    push(&stack, 30);
+    sort(&stack);
+    assert(pop(&stack) == 10);
+    assert(pop(&stack) == 20);
+    assert(pop(&stack) == 30);
+    assert(pop(&stack) == 40);
+    assert(pop(&stack) == 50);
 }
 
 int main() {
